9a6ef3579ef0bd959313bf0bc796e674f202685f
Benjamin Daaki <richarddaaki4@gmail.com>
2024-02-28 19:48:57 +0300

    update - added some landing page template

--- app/ui/welcome/faqs-section.tsx
+++ app/ui/welcome/faqs-section.tsx
@@ -5,10 +5,10 @@ export default function FaqsSection() {
             <h2 className="text-2xl font-bold leading-10 tracking-tight text-gray-900">Frequently asked questions</h2>
             <div className="mt-10 space-y-8 divide-y divide-gray-900/10">
                 <div className="pt-8 lg:grid lg:grid-cols-12 lg:gap-8">
-                    <dt className="text-base font-semibold leading-7 text-gray-900 lg:col-span-5">What's the best thing in tik tok?</dt>
-                    <dd className="mt-4 lg:col-span-7 lg:mt-0">
+                    <div className="text-base font-semibold leading-7 text-gray-900 lg:col-span-5">What's the best thing in tik tok?</div>
+                    <div className="mt-4 lg:col-span-7 lg:mt-0">
                         <p className="text-base leading-7 text-gray-600">The best thing about tik tok is that you can get famous and relax by watching it's videos.</p>
-                    </dd>
+                    </div>
                 </div>
             </div>
         </div>
